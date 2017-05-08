854f6f98e3df31d20c0277792d55b7585944a233
saighin-e <eugen.saighin@gmail.com>
2017-05-08 18:05:28 +0300

    nokogiri integration

--- script.rb
+++ script.rb
@@ -1,6 +1,7 @@
 require 'watir'
 require 'pry-byebug'
 require 'pp'
+require 'nokogiri'
 
 #Create a Transaction class that has the following fields:
 
@@ -22,26 +23,22 @@ CAPTCHA = gets
 
 browser.text_field(name: "Login").set(LOGIN)
 browser.text_field({type: "password"}).set(PASSWORD)
-#browser.text_field(name: "Login").set 'login'
-#browser.text_field({type: "password"}).set('passwd')
 browser.text_field({name: "captchaText"}).set(CAPTCHA)
 
-
-
 #Write a script that navigates through the Victoriabank page and prints an array of objects in the following way:
-
 until browser.span(:class, "owwb-ws-header-user-name").exists? do sleep 1 end
+doc = Nokogiri::HTML(browser.html)
 
 data = {"accounts":
-    [
-      {
-        "name": browser.span(:class, "owwb-ws-header-user-name").text,
-        "balance": browser.span(:class, "owwb-cs-slide-list-amount-value jsMaskedElement").text,
-        "currency": browser.span(:class, "owwb-cs-slide-list-amount-currency").text,
-        "nature": "checking",
-        "transactions": []
-      }
-    ]
+  [
+    {
+      "name": doc.css("span.owwb-ws-header-user-name").text,
+      "balance": doc.css("span.owwb-cs-slide-list-amount-value.jsMaskedElement").first().text,
+      "currency": doc.css("span.owwb-cs-slide-list-amount-currency").first().text,
+      "nature": "checking",
+      "transactions": []
+    }
+  ]
 }
 
 browser.a(:name, "main_menu_CP_HISTORY").click()
@@ -52,17 +49,22 @@ browser.execute_script('document.getElementById("owwb_ws_USER_PROPERTY_DATE_FROM
 browser.form(:id, "owwb_ws_serviceRefreshForm").submit()
 
 #Extend your script in such a way that the stored JSON account will contain a list of Transactions. Example of output:
-browser.lis(:class, "owwb_ws_statementItem owwb_ws_statement_date_item").each do |li|
+doc = Nokogiri::HTML(browser.html)
+date_current = ''
+doc.css("li.owwb_ws_statementItem").each do |li|
 	transaction = Transactions.new
-	date = li.div(:class, "owwb-ws-statement-item-date").div(:class, "owwb-cs-has-tooltip").text
-	time = li.div(:class, "owwb-ws-statement-item-time").text
+  if li.css(".owwb-ws-statement-item-date div.owwb-cs-has-tooltip").length != 0
+    date_current = date = li.css(".owwb-ws-statement-item-date div.owwb-cs-has-tooltip").text.split("\n")[1].strip()
+  else
+    date = date_current
+  end
+
+	time = li.css(".owwb-ws-statement-item-time").text
 	transaction.date = "#{date}T#{time}Z"
-	transaction.description = li.div(:class, "owwb-ws-statement-item-title-wrapper").div(:class, "owwb_ws_statementItemTitle").text
-	transaction.amount = li.span(:class, "jsMaskedElement").text
-	transaction.currency = li.span(:class, "owwb-ws-statement-item-amount-currency").text
+	transaction.description = li.css(".owwb-ws-statement-item-title-wrapper div.owwb_ws_statementItemTitle").text.split("\n")[1].strip()
+	transaction.amount = li.css("span.jsMaskedElement").first().text
+	transaction.currency = li.css("span.owwb-ws-statement-item-amount-currency").first().text
 	data[:accounts][0][:transactions].push(transaction)
 end
 
 pp data
-
-
